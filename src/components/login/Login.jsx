d2e29f23d58f33f4904b51fec39781c14b473176
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2026-02-26 22:08:45 +0000

    Add login and author check

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,69 @@
 // import styles from "./Login.module.css";
 
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { useCheckAuthor } from "../../hooks/useCheckAuthor";
+
 function Login() {
-  return <>Login</>;
+  useCheckAuthor();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  let navigate = useNavigate();
+
+  function updateUsername(e) {
+    setUsername(e.target.value);
+  }
+
+  function updatePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  async function makeLoginAttempt() {
+    try {
+      const res = await fetch("http://localhost:3000/author-api/login", {
+        method: "POST",
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (res.ok) {
+        const result = await res.json();
+
+        localStorage.setItem("token", result.token);
+        localStorage.setItem("user", JSON.stringify(result.user));
+
+        navigate("/");
+      }
+    } catch (error) {
+      console.log("error" + error);
+    }
+  }
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form>
+        <label>
+          Username
+          <input type="text" value={username} onChange={updateUsername} />
+        </label>
+        <label>
+          Password
+          <input type="text" value={password} onChange={updatePassword} />
+        </label>
+        <button type="button" onClick={makeLoginAttempt}>
+          Login
+        </button>
+      </form>
+    </>
+  );
 }
 
 export default Login;
---
+++ src/hooks/useCheckAuthor.js
@@ -0,0 +1,52 @@
+import { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router";
+
+export function useCheckAuthor() {
+  let navigate = useNavigate();
+  let location = useLocation();
+
+  useEffect(() => {
+    const checkAuthor = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        if (location.pathname === "/login") {
+          console.log("no token and return");
+          return;
+        }
+        ("no token and go to login");
+        navigate("/login");
+      }
+
+      try {
+        const res = await fetch("http://localhost:3000/author-api/author", {
+          headers: {
+            Authorization: `bearer ${token}`,
+          },
+        });
+
+        if (res.ok) {
+          console.log("authorised and go home");
+          const result = await res.json();
+          localStorage.setItem("user", JSON.stringify(result.user));
+          navigate("/");
+          return;
+        }
+
+        if (location.pathname === "/login") {
+          console.log("not authorised and return");
+          return;
+        }
+        console.log("not authorised and go back to login");
+        navigate("/login");
+      } catch (error) {
+        console.log("error" + error);
+        if (location.pathname === "/login") {
+          return;
+        }
+        navigate("/login");
+      }
+    };
+
+    checkAuthor();
+  }, [navigate, location]);
+}
