773250cca9be1a4e13094774a3398b8402f40274
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2026-02-28 20:37:33 +0000

    Add post page

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -1,7 +1,104 @@
 // import styles from "./Comment.module.css";
 
-function Comment() {
-  return <></>;
+import { useState } from "react";
+
+function Comment({ comment, commentId, removeComment, editComment }) {
+  const [editing, setEditing] = useState(false);
+
+  async function deleteComment() {
+    const token = localStorage.getItem("token");
+
+    try {
+      const res = await fetch(
+        `http://localhost:3000/author-api/comments/${commentId}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `bearer ${token}`,
+          },
+        },
+      );
+
+      if (res.ok) {
+        console.log("comment deleted");
+        removeComment(commentId);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function updateComment(e) {
+    e.preventDefault();
+    const form = e.currentTarget;
+    console.log(form);
+    const newContent = form.querySelector(".comment-content").value;
+
+    const token = localStorage.getItem("token");
+
+    try {
+      const res = await fetch(
+        `http://localhost:3000/author-api/comments/${commentId}`,
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            content: newContent,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `bearer ${token}`,
+          },
+        },
+      );
+
+      if (res.ok) {
+        console.log("comment updated");
+        const comment = await res.json();
+        editComment(commentId, comment);
+        setEditing(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  console.log(comment);
+
+  return (
+    <>
+      {editing ? (
+        <form onSubmit={updateComment}>
+          <label htmlFor={`edit-content-${commentId}`}>Content</label>
+          <input
+            id={`edit-content-${commentId}`}
+            className="comment-content"
+            type="text"
+            defaultValue={comment.content}
+          />
+          <button type="submit">Update comment</button>
+        </form>
+      ) : (
+        <div>
+          <h4>{comment.user.username}</h4>
+          <p>{comment.content}</p>
+          <p>{comment.uploadedAt}</p>
+        </div>
+      )}
+
+      {!editing && (
+        <div>
+          <button
+            onClick={() => {
+              setEditing(true);
+            }}
+          >
+            Edit
+          </button>
+          <button onClick={deleteComment}>Delete</button>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Comment;
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,7 +1,90 @@
 // import styles from "./Post.module.css";
 
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import { useCheckAuthor } from "../../hooks/useCheckAuthor";
+import Comment from "../comment/Comment";
+
 function Post() {
-  return <>Post</>;
+  useCheckAuthor();
+
+  const location = useLocation();
+  const [, postId] = location.pathname.split("posts/");
+
+  const [post, setPost] = useState();
+
+  useEffect(() => {
+    const getPost = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const res = await fetch(
+          `http://localhost:3000/author-api/posts/${postId}`,
+          {
+            headers: {
+              Authorization: `bearer ${token}`,
+            },
+          },
+        );
+        if (res.ok) {
+          const fetchedPost = await res.json();
+          setPost(fetchedPost);
+        } else {
+          throw new Error("Not received");
+        }
+      } catch (error) {
+        console.log("error" + error);
+      }
+    };
+
+    getPost();
+  }, [postId]);
+
+  function removeComment(commentId) {
+    const newComments = post.comments.filter(
+      (comment) => comment.id !== commentId,
+    );
+
+    setPost({ ...post, comments: [...newComments] });
+  }
+
+  function editComment(commentId, updatedComment) {
+    const updatedComments = post.comments.map((comment) => {
+      if (comment.id === commentId) {
+        return updatedComment;
+      } else {
+        return comment;
+      }
+    });
+
+    setPost({ ...post, comments: [...updatedComments] });
+  }
+
+  return (
+    <>
+      {!post && <div>Loading...</div>}
+      {post && (
+        <div>
+          {" "}
+          <h3>{post.title}</h3>
+          <p>{post.content}</p>
+          <p>Pub: {post.uploadedAt}</p>
+          <p>By {post.author.user.username}</p>
+          <h4>Comments:</h4>
+          {post.comments.map((comment) => {
+            return (
+              <Comment
+                key={comment.id}
+                comment={comment}
+                commentId={comment.id}
+                removeComment={removeComment}
+                editComment={editComment}
+              />
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Post;
